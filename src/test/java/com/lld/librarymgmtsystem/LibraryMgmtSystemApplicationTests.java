a6a3b7fcafd6f60dadfd5ee8030f0aeb79971b49
bhavya <bhavya>
2022-10-17 19:52:21 +0530

    Second commit

---
+++ src/test/java/com/lld/librarymgmtsystem/LibraryMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld.librarymgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
